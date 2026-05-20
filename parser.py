cb5c2c355babad84343669a8eaedf138aadf0963
Harish Senthilkumar <harishsenthilkmr@gmail.com>
2026-05-19 22:50:28 -0700

    read raw course data file

--- parser.py
+++ parser.py
@@ -1,6 +1,6 @@
 # We would like to read the raw file
 def read_raw_file():
-    file_path = "data/raw_data.txt" #original path for raw data
+    file_path = "data/raw_classes.txt" #original path for raw data
 
     #easier to close fine, open file as read mode (exists, so no problem)
     with open(file_path, 'r') as file:
@@ -9,4 +9,11 @@ def read_raw_file():
     return raw_text #send raw text back to caller
 
 def main(): 
+    #Call our helper function to get the file's textual content
     courses_text = read_raw_file() 
+    print(courses_text) #print the raw text to the console
+
+#calls main and the read_raw_file function when the script is executed
+main()
+
+    
